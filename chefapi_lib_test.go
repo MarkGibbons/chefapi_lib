90c09cd1fffc798572acedd5a3d02aa4cc89881b
markgibbons <mark.gibbons@nordstrom.com>
2020-05-06 12:18:10 -0700

    Style fixes

--- chefapi_lib_test.go
+++ chefapi_lib_test.go
@@ -2,43 +2,43 @@ package chefapi_lib
 
 import (
 	"fmt"
+	"github.com/go-chef/chef"
+	"github.com/gorilla/mux"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
 	"testing"
-	"github.com/gorilla/mux"
-	"github.com/go-chef/chef"
 )
 
 const (
-        userid     = "tester"
-        privateKey = "-----BEGIN RSA PRIVATE KEY-----"
+	userid     = "tester"
+	privateKey = "-----BEGIN RSA PRIVATE KEY-----"
 )
 
 var (
-        chefmux *http.ServeMux
-        server  *httptest.Server
-        client  *chef.Client
+	chefmux *http.ServeMux
+	server  *httptest.Server
+	client  *chef.Client
 )
 
 func TestAllOrgs(t *testing.T) {
-        setup()
-        defer teardown()
+	setup()
+	defer teardown()
 
-        chefmux.HandleFunc("/organizations", func(w http.ResponseWriter, r *http.Request) {
-                switch {
-                case r.Method == "GET":
-                        fmt.Fprintf(w, `{ "org_name1": "https://url/for/org_name1", "org_name2": "https://url/for/org_name2"}`)
-                }
-        })
-        want := []string{"org_name1", "org_name2"}
-        orgs, err := AllOrgs()
-        if err != nil {
-                t.Errorf("AllOrgs unexpected error %v\n", err)
-        }
-        if !reflect.DeepEqual(orgs, want) {
-                t.Errorf("Organizations.List returned %+v, want %+v", orgs, want)
-        }
+	chefmux.HandleFunc("/organizations", func(w http.ResponseWriter, r *http.Request) {
+		switch {
+		case r.Method == "GET":
+			fmt.Fprintf(w, `{ "org_name1": "https://url/for/org_name1", "org_name2": "https://url/for/org_name2"}`)
+		}
+	})
+	want := []string{"org_name1", "org_name2"}
+	orgs, err := AllOrgs()
+	if err != nil {
+		t.Errorf("AllOrgs unexpected error %v\n", err)
+	}
+	if !reflect.DeepEqual(orgs, want) {
+		t.Errorf("Organizations.List returned %+v, want %+v", orgs, want)
+	}
 }
 
 // StdMessageStatus extracts the status code from a go-chef api error message
@@ -59,55 +59,55 @@ func TestChefStatus(t *testing.T) {
 }
 
 func TestCleanInput(t *testing.T) {
-        expected_in := map[string]string{"valid": "mynode"}
-        err := CleanInput(expected_in)
-        if err != nil {
-                t.Errorf("Error cleaning: %+v Err: %+v\n", expected_in, err)
-        }
-        expected_in = map[string]string{"invalid": "\nbounceit"}
-        err = CleanInput(expected_in)
-        if err == nil {
-                t.Errorf("CleanInput did not receive expected error cleaning: %+v Err: %+v\n", expected_in, err)
-        }
+	expected_in := map[string]string{"valid": "mynode"}
+	err := CleanInput(expected_in)
+	if err != nil {
+		t.Errorf("Error cleaning: %+v Err: %+v\n", expected_in, err)
+	}
+	expected_in = map[string]string{"invalid": "\nbounceit"}
+	err = CleanInput(expected_in)
+	if err == nil {
+		t.Errorf("CleanInput did not receive expected error cleaning: %+v Err: %+v\n", expected_in, err)
+	}
 }
 
 // inputerror(w *http.ResponseWriter) {
 func TestInputerror(t *testing.T) {
-        // Check the status code and response body - invalid request invoked inputerror
-        req, err := http.NewRequest("GET", "/organizations/other&org/nodes", nil)
-        if err != nil {
-                t.Fatal(err)
-        }
-        rr := httptest.NewRecorder()
-        // Invoke the server
-        newGetNodesServer().ServeHTTP(rr, req)
-        // Check the status code and response body
-        if status := rr.Code; status != http.StatusBadRequest {
-                t.Errorf("Get Nodes status code is not expected. Got: %v want: %v\n", status, http.StatusBadRequest)
-        }
-        wantBody := `{"message":"Bad url input value"}`
-        if rr.Body.String() != wantBody {
-                t.Errorf("Get Nodes unexpected json returned. Expected: %v Got: %v\n", wantBody, rr.Body.String())
-        }
+	// Check the status code and response body - invalid request invoked inputerror
+	req, err := http.NewRequest("GET", "/organizations/other&org/nodes", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	// Invoke the server
+	newGetNodesServer().ServeHTTP(rr, req)
+	// Check the status code and response body
+	if status := rr.Code; status != http.StatusBadRequest {
+		t.Errorf("Get Nodes status code is not expected. Got: %v want: %v\n", status, http.StatusBadRequest)
+	}
+	wantBody := `{"message":"Bad url input value"}`
+	if rr.Body.String() != wantBody {
+		t.Errorf("Get Nodes unexpected json returned. Expected: %v Got: %v\n", wantBody, rr.Body.String())
+	}
 }
 func setup() {
-        chefmux = http.NewServeMux()
-        server = httptest.NewServer(chefmux)
-        client, _ = chef.NewClient(&chef.Config{
-                Name:    userid,
-                Key:     privateKey,
-                BaseURL: server.URL,
-        })
+	chefmux = http.NewServeMux()
+	server = httptest.NewServer(chefmux)
+	client, _ = chef.NewClient(&chef.Config{
+		Name:    userid,
+		Key:     privateKey,
+		BaseURL: server.URL,
+	})
 }
 
 func teardown() {
-        server.Close()
+	server.Close()
 }
 
 func newGetNodesServer() http.Handler {
-        r := mux.NewRouter()
-        r.HandleFunc("/organizations/{org}/nodes", getNodes)
-        return r
+	r := mux.NewRouter()
+	r.HandleFunc("/organizations/{org}/nodes", getNodes)
+	return r
 }
 
 func getNodes(w http.ResponseWriter, r *http.Request) {
